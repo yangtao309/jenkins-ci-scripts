eb7b46b8e715d5d36d8c3643af153635da7f85ad
涛 杨 <tao.yang@shuyun.com>
2016-09-30 11:00:36 +0800

    fix it

---
+++ pipeline-dev.groovy
@@ -0,0 +1,52 @@
+#!groovy
+
+#####
+# dev environment pipeline settings
+####
+
+stage('Code Quality')
+node { echo 'code quality execute' }
+parallel(CheckStyle: {
+    node {
+        sh 'gradle checkstyleMain'
+        step([$class: 'CheckStylePublisher', canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/checkstyle/main.xml', unHealthy: ''])
+    }
+}, FindBugs: {
+    node {
+        checkout scm
+        unstash 'scripts'
+        sh 'sh reset-build-gradle.sh'
+        sh 'gradle findbugsMain'
+        step([$class: 'FindBugsPublisher', canComputeNew: false, defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', pattern: '**/findbugs/main.xml', unHealthy: ''])
+    }
+}, Pmd: {
+    node {
+        sh 'gradle pmdMain'
+        step([$class: 'PmdPublisher', canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/pmd/main.xml', unHealthy: ''])
+    }
+})
+
+
+stage('Unit Test')
+node { sh 'gradle clean test' }
+parallel(TestNG: {
+    node {
+        echo 'testng test'
+        step([$class: 'Publisher', reportFilenamePattern: '**/reports/testng/testng-results.xml'])
+    }
+}, Junit: {
+     node {
+        checkout scm
+        unstash 'scripts'
+        sh 'sh reset-build-gradle.sh'
+        sh 'gradle clean test'
+        echo 'junit test'
+        step([$class: 'JUnitResultArchiver', testResults: '**/test-results/TEST-*.xml'])
+    }
+}, Jacoco: {
+    node {
+        echo 'jacoco reports generator'
+    }
+})
+
+
